7354d1694a9a201dd0fa23eb1cbdbf44326b4390
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-01 17:48:57 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,11 @@ def first_challenge
 contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      value.delete_if do |flavour| flavour ==
+      value.delete_if do |flavour| flavour =="strawberry"
+      
+      end
+    end
+    
   end
 
 end
